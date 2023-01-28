4fe32859f73aebe9077d55bfcc446fba720062e0
lucas-gio <gioialucasf@gmail.com>
2023-01-28 16:13:36 -0300

    · Radio stations icons and size review.

--- src/main/kotlin/com/gioia/radio/ui/screens/stations/Stations.kt
+++ src/main/kotlin/com/gioia/radio/ui/screens/stations/Stations.kt
@@ -1,19 +1,28 @@
 package com.gioia.radio.ui.screens.stations
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DoubleArrow
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.More
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.gioia.radio.App
@@ -41,10 +50,24 @@ fun Stations(
                     .clickable { stationsViewModel.onRadioSelected(radioStation) }
                     .fillMaxWidth()
             ) {
-                Column(verticalArrangement = Arrangement.spacedBy(10.dp),)
+                Column(verticalArrangement = Arrangement.spacedBy(10.dp))
                 {
 
-                    Text(text = radioStation.name, style = AppTypography.subtitle1)
+                    Row(horizontalArrangement = Arrangement.spacedBy(15.dp)){
+                        Card(
+                            modifier = Modifier.size(45.dp),
+                            shape = CircleShape,
+                        ) {
+                            Image(
+                                imageVector = Icons.Default.Search,
+                                "",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.size(20.dp).border(1.dp, Color.Black, CircleShape)
+                            )
+                        }
+
+                        Text(text = radioStation.name, style = AppTypography.subtitle1)
+                    }
                     radioStation.category?.let {
                         Text(
                             text = it,
@@ -64,9 +87,13 @@ fun Stations(
                         onClick = {
                             App.rootComponent.onStationDetail(radioStation)
                         },
-                        shape = CircleShape,
+                        shape = RoundedCornerShape(20.dp)
                     ) {
-                        Icon(Icons.Rounded.ArrowForward ,contentDescription = "Radio info")
+                        Icon(
+                            Icons.Default.DoubleArrow ,
+                            contentDescription = "Radio info",
+                            modifier = Modifier.size(20.dp)
+                        )
                     }
                     Divider()
                 }
